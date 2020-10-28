309e61dcfd6be79f554b1529e7c9e62c925303f7
hchulim <hchulim@smilegate.com>
2020-10-28 20:43:22 +0900

    -- 서버별 환경변수 설정 및 로그설정 추가

--- src/main/kotlin/com/hclim1/common/demo/example/contoroller/ExampleController.kt
+++ src/main/kotlin/com/hclim1/common/demo/example/contoroller/ExampleController.kt
@@ -2,12 +2,14 @@ package com.hclim1.common.demo.example.contoroller
 
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("/example")
 class ExampleController {
 
-	@GetMapping("/example")
+	@GetMapping("")
 	fun getExample(): String {
 		return "Test Example"
 	}
